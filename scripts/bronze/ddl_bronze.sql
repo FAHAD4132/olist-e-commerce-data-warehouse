9de6e4ace05dd3ac73df1b43d9dbc1733e4368a9
FAHAD4132 <165099842+FAHAD4132@users.noreply.github.com>
2025-11-24 14:16:17 +0300

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -14,16 +14,16 @@ IF OBJECT_ID ('Bronze.CRM_Fecom_Inc_Customer_List', 'U') IS NOT NULL
 GO
   
 CREATE TABLE Bronze.CRM_Fecom_Inc_Customer_List (
-	Customer_Trx_ID NVARCHAR(50), 
-	Subscriber_ID NVARCHAR(50), 
-	Subscribe_Date DATE, 
-	First_Order_Date DATE, 
-	Customer_Postal_Code NVARCHAR(20),
-	Customer_City NVARCHAR(50),
-	Customer_Country NVARCHAR(50),
+	Customer_Trx_ID		  NVARCHAR(50), 
+	Subscriber_ID		  NVARCHAR(50), 
+	Subscribe_Date 		  DATE, 
+	First_Order_Date 	  DATE, 
+	Customer_Postal_Code  NVARCHAR(20),
+	Customer_City		  NVARCHAR(50),
+	Customer_Country	  NVARCHAR(50),
 	Customer_Country_Code NVARCHAR(5),
-	Age INT,
-	Gender NVARCHAR(10)
+	Age					  INT,
+	Gender				  NVARCHAR(10)
 );
 GO
   
@@ -32,11 +32,11 @@ IF OBJECT_ID ('Bronze.CRM_Fecom_Inc_Geolocations', 'U') IS NOT NULL
 GO
   
 CREATE TABLE Bronze.CRM_Fecom_Inc_Geolocations (
-	Geo_Postal_Code NVARCHAR(20), 
-	Geo_Lat NVARCHAR(50), 
-	Geo_Lon NVARCHAR(50), 
+	Geo_Postal_Code  NVARCHAR(20), 
+	Geo_Lat 		 NVARCHAR(50), 
+	Geo_Lon 		 NVARCHAR(50), 
 	Geolocation_City NVARCHAR(100), 
-	Geo_Country NVARCHAR(100)
+	Geo_Country 	 NVARCHAR(100)
 );
 GO
   
@@ -45,13 +45,13 @@ IF OBJECT_ID ('Bronze.CRM_Fecom_Inc_Order_Reviews', 'U') IS NOT NULL
 GO
   
 CREATE TABLE Bronze.CRM_Fecom_Inc_Order_Reviews (
-	Review_ID NVARCHAR(50), 
-	Order_ID NVARCHAR(50), 
-	Review_Score INT, 
-	Review_Comment_Title_En NVARCHAR(250), 
+	Review_ID				  NVARCHAR(50), 
+	Order_ID				  NVARCHAR(50), 
+	Review_Score 			  INT, 
+	Review_Comment_Title_En   NVARCHAR(250), 
 	Review_Comment_Message_En NVARCHAR(MAX),
-	Review_Creation_Date DATETIME,
-	Review_Answer_Timestamp DATETIME
+	Review_Creation_Date 	  DATETIME,
+	Review_Answer_Timestamp   DATETIME
 );
 GO
 
@@ -60,13 +60,13 @@ IF OBJECT_ID ('Bronze.ERP_Fecom_Inc_Order_Items', 'U') IS NOT NULL
 GO
   
 CREATE TABLE Bronze.ERP_Fecom_Inc_Order_Items (
-	Order_ID NVARCHAR(50), 
-	Order_Item_ID INT, 
-	Product_ID NVARCHAR(50), 
-	Seller_ID NVARCHAR(50), 
+	Order_ID 			NVARCHAR(50), 
+	Order_Item_ID 		INT, 
+	Product_ID 			NVARCHAR(50), 
+	Seller_ID 			NVARCHAR(50), 
 	Shipping_Limit_Date DATETIME,
-	Price DECIMAL(10,2),
-	Freight_Value DECIMAL(10,2)
+	Price 				DECIMAL(10,2),
+	Freight_Value 		DECIMAL(10,2)
 );
 GO
 
@@ -75,11 +75,11 @@ IF OBJECT_ID ('Bronze.ERP_Fecom_Inc_Order_Payments', 'U') IS NOT NULL
 GO
   
 CREATE TABLE Bronze.ERP_Fecom_Inc_Order_Payments (
-	Order_ID NVARCHAR(50), 
-	Payment_Sequential INT, 
-	Payment_Type NVARCHAR(50), 
+	Order_ID 			 NVARCHAR(50), 
+	Payment_Sequential 	 INT, 
+	Payment_Type 		 NVARCHAR(50), 
 	Payment_Installments INT, 
-	Payment_Value DECIMAL(10,2)
+	Payment_Value 		 DECIMAL(10,2)
 );
 GO
 
@@ -88,12 +88,12 @@ IF OBJECT_ID ('Bronze.ERP_Fecom_Inc_Orders', 'U') IS NOT NULL
 GO
   
 CREATE TABLE Bronze.ERP_Fecom_Inc_Orders (
-	Order_ID NVARCHAR(50), 
-	Customer_Trx_ID NVARCHAR(50), 
-	Order_Status NVARCHAR(20), 
-	Order_Purchase_Timestamp DATETIME, 
-	Order_Approved_At DATETIME,
-	Order_Delivered_Carrier_Date DATETIME,
+	Order_ID					  NVARCHAR(50), 
+	Customer_Trx_ID				  NVARCHAR(50), 
+	Order_Status				  NVARCHAR(20), 
+	Order_Purchase_Timestamp	  DATETIME, 
+	Order_Approved_At 			  DATETIME,
+	Order_Delivered_Carrier_Date  DATETIME,
 	Order_Delivered_Customer_Date DATETIME,
 	Order_Estimated_Delivery_Date DATETIME
 );
@@ -104,12 +104,12 @@ IF OBJECT_ID ('Bronze.ERP_Fecom_Inc_Products', 'U') IS NOT NULL
 GO
   
 CREATE TABLE Bronze.ERP_Fecom_Inc_Products (
-	Product_ID NVARCHAR(50), 
+	Product_ID			  NVARCHAR(50), 
 	Product_Category_Name NVARCHAR(50), 
-	Product_Weight_Gr INT, 
-	Product_Length_Cm INT, 
-	Product_Height_Cm INT,
-	Product_Width_Cm INT
+	Product_Weight_Gr 	  INT, 
+	Product_Length_Cm	  INT, 
+	Product_Height_Cm	  INT,
+	Product_Width_Cm	  INT
 );
 GO
 
@@ -118,11 +118,11 @@ IF OBJECT_ID ('Bronze.ERP_Fecom_Inc_Sellers_List', 'U') IS NOT NULL
 GO
   
 CREATE TABLE Bronze.ERP_Fecom_Inc_Sellers_List (
-	Seller_ID NVARCHAR(50), 
-	Seller_Name NVARCHAR(50), 
+	Seller_ID		   NVARCHAR(50), 
+	Seller_Name 	   NVARCHAR(50), 
 	Seller_Postal_Code NVARCHAR(20), 
-	Seller_City NVARCHAR(50), 
-	Country_Code NVARCHAR(5),
-	Seller_Country NVARCHAR(50)
+	Seller_City		   NVARCHAR(50), 
+	Country_Code	   NVARCHAR(5),
+	Seller_Country	   NVARCHAR(50)
 );
 GO
